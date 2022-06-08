4a9941c7f2f6e97edfcfbcecf3f9f8eef301eb60
brandformers <40883156+timnijssen@users.noreply.github.com>
2022-06-08 11:47:40 +0200

    Changed error message

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,16 +1,18 @@
 @if ($errors->any() || session('status') || session('error'))
     <div class="px-6 py-3 text-left w-full mx-auto bg-gray-100 border-t border-gray-200">
         <div class="px-10 w-full sm:max-w-7xl mx-auto">
-            <p class="font-bold text-xl">Whoops...</p>
             @if ($errors->any())
+                <p class="font-bold text-xl">Whoops...</p>
                 @foreach ($errors->all() as $error)
                     <p class="text-red-500 font-semibold">{{ $error }}</p>
                 @endforeach
             @endif
             @if (session('status'))
+                <p class="font-bold text-xl">Yes!</p>
                 <p class="text-red-500 font-semibold">{{ session('status') }}</p>
             @endif
             @if (session('error'))
+                <p class="font-bold text-xl">Whoops...</p>
                 <p class="text-red-500 font-semibold">{{ session('error') }}</p>
             @endif
         </div>
