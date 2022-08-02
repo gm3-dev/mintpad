0ff35728c7bfdaac6e77fe3d07e120e267e34e8c
brandformers <40883156+timnijssen@users.noreply.github.com>
2022-08-02 16:02:17 +0200

    Fixed typo

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/collection';
+    public const HOME = '/collections';
 
     /**
      * The controller namespace for the application.
