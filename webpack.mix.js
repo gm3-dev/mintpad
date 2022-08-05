ffeb1679e8198255a130b9a80482de78e4d6551e
brandformers <40883156+timnijssen@users.noreply.github.com>
2022-08-05 09:17:39 +0200

    Changed webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,17 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
-/*const webpack = require('webpack')
+const webpack = require('webpack')
 
-mix.webpackConfig ({
-    plugins: [
-        new webpack.DefinePlugin({
-            __VUE_OPTIONS_API__: false,
-            __VUE_PROD_DEVTOOLS__: false,
-        }),
-    ],
-})*/
+if (mix.inProduction()) {
+    mix.webpackConfig ({
+        plugins: [
+            new webpack.DefinePlugin({
+                __VUE_OPTIONS_API__: false,
+                __VUE_PROD_DEVTOOLS__: false,
+            }),
+        ],
+    })
+}
 
 /*
  |--------------------------------------------------------------------------
