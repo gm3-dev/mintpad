e327fc6ba2f26a644494b4842ffa0c16f968dd99
brandformers <40883156+timnijssen@users.noreply.github.com>
2022-09-02 10:53:35 +0200

    Fixes #31: added bigger collection page button

--- resources/views/collections/edit.blade.php
+++ resources/views/collections/edit.blade.php
@@ -213,8 +213,7 @@
             </div>
             <div v-if="page.tab == 4">
                 <h3 class="text-2xl mb-4 mt-6">{{ __('Mint settings') }}</h3>
-                <p class="text-mintpad-300 font-regular text-sm">{{ __('Here you can add some information about your collection that will be shown on your collections mint page.') }}</p>
-                <p class="text-mintpad-300 font-regular text-sm mb-4">{{ __('You can find the mint page for this collection') }} <a href="{{ route('mint.index', $collection->id) }}" target="_blank" class="font-semibold">here</a>.</p>
+                <p class="text-mintpad-300 font-regular text-sm mb-4">{{ __('Here you can add some information about your collection that will be shown on your collections mint page.') }}</p>
 
                 <div class="w-full grid grid-cols-2 gap-2 mb-6">
                     <div>
@@ -242,7 +241,8 @@
                 </div>
 
                 <div class="w-full">
-                    <x-button href="#" @click.prevent="updateMintSettings">{{ __('Update mint settings') }}</x-button>
+                    <x-link-button href="#" @click.prevent="updateMintSettings">{{ __('Update mint settings') }}</x-link-button>
+                    <x-gray-button href="{{ route('mint.index', $collection->id) }}" target="_blank" class="ml-2">{{ __('View collection page') }}</x-gray-button>
                 </div>
             </div>
         </div>
