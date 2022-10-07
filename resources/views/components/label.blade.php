14cc0219830ac3f8b72b2783fd88c9c1b4609915
brandformers <40883156+timnijssen@users.noreply.github.com>
2022-10-07 12:33:39 +0200

    Fixed label issue

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,7 +1,7 @@
 @props(['value' => false, 'info' => false])
 
 <label {{ $attributes->merge(['class' => 'inline-block text-sm text-mintpad-400 dark:text-gray-200']) }}>
-    {{ $value ?? $slot }}
+    {{ $value ? $value : $slot }}
 </label>
 @if ($info)
 <x-more-info content="{{ $info }}"></x-more-info>
