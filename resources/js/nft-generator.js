131b0ebcd41a3834877946af2ebe25e7e085d0cd
brandformers <40883156+timnijssen@users.noreply.github.com>
2022-10-19 14:33:08 +0200

    Added disconnect reason for socketio

--- resources/js/nft-generator.js
+++ resources/js/nft-generator.js
@@ -43,8 +43,8 @@ export default {
         socket.on('connect', () => {
             console.log('connect', socket.id)
         })
-        socket.on('disconnect', () => {
-            console.log('disconnect', socket.id)
+        socket.on('disconnect', (reason) => {
+            console.log('disconnect', reason)
         })
     },
     methods: {
