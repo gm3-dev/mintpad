f3116af1ec78c7b01c928c650ce5bda89e80ee9d
brandformers <40883156+timnijssen@users.noreply.github.com>
2022-11-03 16:31:10 +0100

    Deploy trigger

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -86,7 +86,6 @@ if (document.getElementById('app')) {
             if ($('#collectionID').length) {
                 this.collectionID = $('#collectionID').val()
             }
-
             axios.get('/'+this.collectionID+'/fetch').then(async (response) => {
                 this.collection.buttons = response.data.buttons
                 this.collection.about = response.data.about
