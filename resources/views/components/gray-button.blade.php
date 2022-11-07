4e905e478cd4d851dbeb72d248c56b553d97e7c4
brandformers <40883156+timnijssen@users.noreply.github.com>
2022-11-07 21:22:39 +0100

    Removed loader icon from button

--- resources/views/components/gray-button.blade.php
+++ resources/views/components/gray-button.blade.php
@@ -1,4 +1,3 @@
-<a {{ $attributes->merge(['class' => 'inline-block relative px-4 lg:px-12 py-2 text-sm bg-mintpad-200 dark:bg-mintpad-400 text-mintpad-300 dark:text-mintpad-200 hover:text-mintpad-400 border border-transparent rounded-lg text-sm text-center disabled:opacity-25 transition ease-in-out duration-150']) }}>
-    <i class="absolute left-2 mt-[3px] fa-solid fa-gear animate-spin" style="display: none;"></i>
+<a {{ $attributes->merge(['class' => 'inline-block px-4 lg:px-12 py-2 text-sm bg-mintpad-200 dark:bg-mintpad-400 text-mintpad-300 dark:text-mintpad-200 hover:text-mintpad-400 border border-transparent rounded-lg text-sm text-center disabled:opacity-25 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </a>
