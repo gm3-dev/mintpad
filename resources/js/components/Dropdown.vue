9c2d068d31045fd63230c0759e5544a1f3f3d4d2
brandformers <40883156+timnijssen@users.noreply.github.com>
2023-02-24 16:46:17 +0100

    Small console error fix

--- resources/js/components/Dropdown.vue
+++ resources/js/components/Dropdown.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 
-defineProps({
+const props = defineProps({
     refname: String
 })
 
@@ -20,7 +20,7 @@ function hide() {
             <slot name="button"></slot>
         </button>
 
-        <div v-show="open" v-closable="{exclude: [this.refname], handler: 'hide'}" class="absolute z-50 mt-2 w-48 rounded-md origin-top-right right-0 dark:border dark:border-mintpad-900">
+        <div v-show="open" v-closable="{exclude: [refname], handler: 'hide'}" class="absolute z-50 mt-2 w-48 rounded-md origin-top-right right-0 dark:border dark:border-mintpad-900">
             <div class="rounded-md ring-1 ring-black ring-opacity-5 bg-white dark:bg-mintpad-800">
                 <slot name="links"></slot>  
             </div>
