15d6e99bd558cdbd8bb4d81d9e0ca5c2ebc09485
brandformers <40883156+timnijssen@users.noreply.github.com>
2023-03-29 10:36:50 +0200

    Added routed link to logo

--- resources/js/Components/Logo.vue
+++ resources/js/Components/Logo.vue
@@ -3,7 +3,7 @@ import { Link } from '@inertiajs/vue3'
 
 </script>
 <template>
-    <Link href="/" class="header-logo relative text-2xl font-jpegdev text-mintpad-700">
+    <Link :href="route('collections.index')" class="header-logo relative text-2xl font-jpegdev text-mintpad-700">
         <img src="/images/logo.svg" class="h-[35px] darkmode-off" />
         <img src="/images/logo-white.svg" class="h-[35px] darkmode-on" />
     </Link>
