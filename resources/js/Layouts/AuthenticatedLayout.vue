0fc269c5f33c12b7b9dca00ad021cc7318f85085
brandformers <40883156+timnijssen@users.noreply.github.com>
2023-03-29 10:12:20 +0200

    Removed admin bar

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -22,12 +22,6 @@ const switchBlockchain = async () => {
 </script>
 <template>
     <div class="main-container min-h-screen bg-primary-100 dark:bg-mintpad-500">
-        <!-- Admin bar -->
-        <div v-if="route().current('admin.*')" class="w-full bg-primary-600 text-center text-sm">
-            <div class="max-w-7xl mx-auto py-1 px-4 sm:px-6">
-                <p class="text-white dark:text-white mb-0">You are logged in as admin</p>
-            </div>
-        </div>
         <Navigation />
 
         <div v-if="overlay" class="fixed z-40 inset-0 bg-gray-200 dark:bg-mintpad-500 bg-opacity-50 dark:bg-opacity-50 transition-opacity"></div>
