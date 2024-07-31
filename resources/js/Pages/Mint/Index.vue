b93b4d70e7fb87a27e406516e03b66539a049c5d
Rilsosing <53212931+chimmykk@users.noreply.github.com>
2024-07-31 22:23:45 +0530

    fixes chain id and polling transaction for taiko

--- resources/js/Pages/Mint/Index.vue
+++ resources/js/Pages/Mint/Index.vue
@@ -376,12 +376,12 @@ const mintNFT = async () => {
         };
 
         // conditional polling trigger based on chainId
-        const chainId = 167009;
+        const chainId = 167000;
         console.log(props.collection.chain_id);
         if (chainId === props.collection.chain_id) {
          // trigger the polling
          // check for cors origin later
-            const response = await fetch('https://http://127.0.0.1/.xyz/startPolling', {
+            const response = await fetch('https://on.mintpad.co/startPolling', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
