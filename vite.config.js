6766d2578401733568f9388001bc320e62ab9649
Rilsosing <53212931+chimmykk@users.noreply.github.com>
2024-10-24 02:49:17 +0530

    Update vite.config.js
    update host

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import fs from 'fs';
 
-const host = 'app.mintpad.test';
+const host = '143.244.157.67';
 
 export default ({ mode }) => {
     process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
