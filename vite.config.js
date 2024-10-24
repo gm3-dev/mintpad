e452959b06022270256dc6f263612f8e5d2cf9b3
Rilsosing <53212931+chimmykk@users.noreply.github.com>
2024-10-24 20:08:58 +0530

    Update vite.config.js
    use domain

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig, loadEnv } from 'vite';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import fs from 'fs';
-const host = '127.0.0.1';
+const host = 'semjjonline.xyz';
 
 export default ({ mode }) => {
     process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
-
+// etst
     let server = null;
-    if (process.env.APP_ENV == 'local') {
+    if (process.env.APP_ENV === 'local') {
         server = {
             host,
             hmr: { host },
             https: {
-                key: fs.readFileSync(`/Applications/MAMP/Library/OpenSSL/certs/${host}.key`),
-                cert: fs.readFileSync(`/Applications/MAMP/Library/OpenSSL/certs/${host}.crt`),
+                key: fs.readFileSync('./app/ssl/localhost.key'),
+                cert: fs.readFileSync('./app/ssl/localhost.crt'),
             },
         };
     }
@@ -34,7 +34,7 @@ export default ({ mode }) => {
             }),
             laravel({
                 input: [
-                    'resources/sass/app.scss', 
+                    'resources/sass/app.scss',
                     'resources/js/app.js'
                 ],
                 refresh: true,
@@ -57,4 +57,4 @@ export default ({ mode }) => {
             },
         },
     });
-}
+};
